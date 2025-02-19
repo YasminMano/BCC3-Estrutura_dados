b4af5b3c16019763752d5db02b5563f0d3a672a7
YASMIN AYUMI FOLTRAN MANO <yasmin.afmano@senacsp.edu.br>
2025-02-19 20:02:59 -0300

    Segunda versão exercício 02.02

--- Exercicio_002.c
+++ Exercicio_002.c
@@ -8,26 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	int i, n = 10;
-	int vetor[n];
-	for (i = 0; i < n; i++) {
-		vetor[i] = rand() % 100;
-	}
-	printf("Vetor desordenado pseudo aleatorio:\n");
-	for (i = 0; i < n; i++) {
-		printf("%d ", vetor[i]);
-	}
-	printf("\n");
-	bubbleSort(vetor, n);
-	printf("Vetor ordenado:\n");
-	for (i = 0; i < n; i++) {
-		printf("%d ", vetor[i]);
-	}
-	printf("\n");
-	return 0;
-}
-
 void bubbleSort(int vetor[], int n) {
 	int i, aux;
 
@@ -44,4 +24,24 @@ void bubbleSort(int vetor[], int n) {
 	}
 
 	bubbleSort(vetor, n - 1);
+}
+
+int main() {
+	int i, n = 10;
+	int vetor[n];
+	for (i = 0; i < n; i++) {
+		vetor[i] = rand() % 100;
+	}
+	printf("Vetor desordenado:\n");
+	for (i = 0; i < n; i++) {
+		printf("%d ", vetor[i]);
+	}
+	printf("\n");
+	bubbleSort(vetor, n);
+	printf("Vetor ordenado:\n");
+	for (i = 0; i < n; i++) {
+		printf("%d ", vetor[i]);
+	}
+	printf("\n");
+	return 0;
 }
