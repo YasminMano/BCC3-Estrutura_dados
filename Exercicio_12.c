920270f694ad1974e0c7e00955216b57c682f7fa
YasminMano <135649752+YasminMano@users.noreply.github.com>
2025-05-07 20:04:13 -0300

    Create Exercicio_12.c

---
+++ Exercicio_12.c
@@ -0,0 +1,149 @@
+// Exercício 12
+// Modifique a implementação da tabela hash com
+// endereçamento aberto para utilizar tentativa quadrática
+// como método de resolução de colisões.
+
+// Código Fonte:
+
+// int hash(int mat, int m, int k) {
+//     return (hash_linha(mat, m) + k) % m; // Tentativa (sondagem) linear
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Aluno *link;
+
+struct Aluno {
+    int matricula; // Número de matrícula do aluno
+    float cr;      // Coeficiente de Rendimento
+};
+
+typedef struct Aluno *Hash; // Hash é um vetor de ponteiros para structs do tipo Aluno
+
+void inicializar(Hash *tab, int m) {
+    for (int i = 0; i < m; i++) {
+        tab[i] = NULL;
+    }
+}
+
+int hash_linha(int mat, int m) {
+    return mat % m; // Método da divisão
+}
+
+// Agora com tentativa quadrática
+int hash(int mat, int m, int k) {
+    return (hash_linha(mat, m) + k * k) % m;
+}
+
+int buscar(Hash *tab, int m, int mat, int *achou) {
+    *achou = 0;
+    int endereco = -1;
+    int posicao_livre = -1;
+    int k = 0;
+
+    while (k < m) {
+        endereco = hash(mat, m, k);
+        if (tab[endereco] != NULL && tab[endereco]->matricula == mat) {
+            *achou = 1;
+            return endereco;
+        } else {
+            if (tab[endereco] == NULL && posicao_livre == -1) {
+                posicao_livre = endereco;
+            }
+        }
+        k++;
+    }
+
+    return posicao_livre;
+}
+
+void inserir(Hash *tab, int m, int mat, float cr) {
+    int achou;
+    int endereco = buscar(tab, m, mat, &achou);
+    if (!achou) {
+        if (endereco != -1) {
+            tab[endereco] = (link) malloc(sizeof(struct Aluno));
+            tab[endereco]->matricula = mat;
+            tab[endereco]->cr = cr;
+        } else {
+            printf("A tabela está cheia. Ocorreu overflow. Inserção não realizada!\n");
+        }
+    } else {
+        printf("Matrícula já existe. Inserção inválida!\n");
+    }
+}
+
+void remover(Hash *tab, int m, int mat) {
+    int achou;
+    int endereco = buscar(tab, m, mat, &achou);
+    if (achou) {
+        free(tab[endereco]);
+        tab[endereco] = NULL;
+    } else {
+        printf("Matrícula não encontrada - Remoção não realizada!\n");
+    }
+}
+
+void imprimir(Hash *tab, int m) {
+    for (int i = 0; i < m; i++) {
+        if (tab[i] != NULL) {
+            link p = tab[i];
+            printf("tab[%d]:\tmat:%d\tcr:%.2f\n", i, p->matricula, p->cr);
+        } else {
+            printf("tab[%d]:\n", i);
+        }
+    }
+}
+
+int main(void) {
+    int n, m;
+    printf("Digite o valor de m (tamanho da tabela hash): ");
+    scanf("%d", &m);
+    printf("Digite o valor de n (quantidade de matriculas): ");
+    scanf("%d", &n);
+
+    Hash tab[m];
+    inicializar(tab, m);
+    imprimir(tab, m);
+
+    int mat;
+    float cr;
+    for (int i = 0; i < n; i++) {
+        printf("Digite a matricula do aluno %d: ", i + 1);
+        scanf("%d", &mat);
+        printf("Digite o CR do aluno %d: ", i + 1);
+        scanf("%f", &cr);
+        inserir(tab, m, mat, cr);
+        imprimir(tab, m);
+    }
+
+    char resposta;
+    do {
+        printf("Digite a matricula que deseja procurar: ");
+        scanf("%d", &mat);
+        int achou;
+        int endereco = buscar(tab, m, mat, &achou);
+        if (!achou) {
+            printf("Matrícula não encontrada!\n");
+        } else {
+            link p = tab[endereco];
+            printf("Encontrou mat = %d , cr = %.2f\n", p->matricula, p->cr);
+        }
+        printf("Quer procurar outra matrícula? (s/n) ");
+        scanf(" %c", &resposta);
+    } while (resposta != 'n' && resposta != 'N');
+
+    imprimir(tab, m);
+
+    do {
+        printf("Digite a matricula que deseja remover: ");
+        scanf("%d", &mat);
+        remover(tab, m, mat);
+        imprimir(tab, m);
+        printf("Quer remover outra matrícula? (s/n) ");
+        scanf(" %c", &resposta);
+    } while (resposta != 'n' && resposta != 'N');
+
+    return 0;
+}
